03c6e08310830bc8989a0fee1d2da2c415665284
Abbas <i202404@nu.edu.pk>
2023-10-01 17:35:51 +0500

    test #2

--- task.py
+++ task.py
@@ -32,7 +32,7 @@ mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 print(f'Mean Squared Error: {mse:.2f}')
-print(f'R-squared (Coefficient of Determination): {r2:.2f}')
+# print(f'R-squared (Coefficient of Determination): {r2:.2f}')
 
 
 # plt.scatter(X_test, y_test, color='black')
