cbd350ec76ffc09084fbbc837c94595e1f50fa77
Abbas <i202404@nu.edu.pk>
2023-10-01 17:31:01 +0500

    updated files to test workflow

--- task.py
+++ task.py
@@ -4,6 +4,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
 
 # Load the Iris dataset
 iris = datasets.load_iris()
@@ -33,12 +34,10 @@ r2 = r2_score(y_test, y_pred)
 print(f'Mean Squared Error: {mse:.2f}')
 print(f'R-squared (Coefficient of Determination): {r2:.2f}')
 
-# Visualize the regression line (optional)
-import matplotlib.pyplot as plt
 
-plt.scatter(X_test, y_test, color='black')
-plt.plot(X_test, y_pred, color='blue', linewidth=3)
-plt.xlabel('Sepal Length (cm)')
-plt.ylabel('Target')
-plt.title('Linear Regression on Sepal Length')
-plt.show()
+# plt.scatter(X_test, y_test, color='black')
+# plt.plot(X_test, y_pred, color='blue', linewidth=3)
+# plt.xlabel('Sepal Length (cm)')
+# plt.ylabel('Target')
+# plt.title('Linear Regression on Sepal Length')
+# plt.show()
